42982cb9ee54849eaa15fc486d8457d5070e92ca
moroz <morozarturv@gmail.com>
2014-03-18 17:59:01 +0200

    home

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+$(window).scroll(function(){
+	var scroll_top = $(document).scrollTop();
+	if (scroll_top > 0) {
+		$('.header').addClass('is-fixed');
+	}
+	else{
+		$('.header').removeClass('is-fixed');
+	}
+});
+
 });
